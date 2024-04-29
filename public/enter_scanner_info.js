e5f2a983ffdc714dfa74f4b52e8a6ee7f4f926f5
Tom Hicks <hickst@tohono.com>
2024-04-28 17:55:14 -0700

    Update with latest infopoint imports (from test page).

---
+++ public/enter_scanner_info.js
@@ -0,0 +1,22 @@
+var tourInfo = {
+  "MRI": {
+    "title": "MRI Research Scanner",
+    "image": "/public/images/es_dot_control.jpg",
+    "body": "The MRI Research scanner is a Siemens Skyra 3T machine. The small display above the scanner bore is called the DOT (Day Optimized Throughput) and shows status information concerning patient position, connected coils and more."
+  },
+  "MRIbore": {
+    "title": "Scanner Bore",
+    "body": "The research subject is scanned inside this cavity."
+  },
+  "physioCart": {
+    "title": "Physiological Cart",
+    "image": "/public/images/es_physio_cart.jpg",
+    "body": "Physiological monitoring equipment for participant pulse, CO2, blood pressure.  This works together with the wireless Philips Expression IP5 display at the Syngo workstation.<br> Model name and number: Philips Expression MR400"
+  },
+  "NO_TITLE_BAD": {
+    "body": "Oops, this should have a TITLE."
+  },
+  "NO_BODY_BAD": {
+    "title": "Oops, this should have a BODY."
+  },
+};
