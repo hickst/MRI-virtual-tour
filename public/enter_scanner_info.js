a4e61addf0750b2ab14f71eee1d3224e869d8267
Tom Hicks <hickst@tohono.com>
2024-04-28 18:35:15 -0700

    Add bore and coil info and image.

--- public/enter_scanner_info.js
+++ public/enter_scanner_info.js
@@ -5,13 +5,14 @@ var tourInfo = {
     "body": "The MRI Research scanner is a Siemens Skyra 3T machine. The small display above the scanner bore is called the DOT (Day Optimized Throughput) and shows status information concerning patient position, connected coils and more."
   },
   "MRIbore": {
-    "title": "Scanner Bore",
-    "body": "The research subject is scanned inside this cavity."
+    "title": "Scanner Bore and Coils",
+    "image": "/public/images/es_bore_coil.png",
+    "body": "The research subject is scanned inside this central tube, called the 'bore'. At the opening of the bore, you can see two different coils, which are antennas that receive radio frequencies from your body and transmit data to the computer. The head coil (white) is a hard coil that fits like a cage around the participant's head. The red coil is soft and lies on top of the participant. We have a variety of different coils for different body parts."
   },
   "physioCart": {
-    "title": "Physiological Cart",
+    "title": "Physiological Monitoring Cart",
     "image": "/public/images/es_physio_cart.jpg",
-    "body": "Physiological monitoring equipment for participant pulse, CO2, blood pressure.  This works together with the wireless Philips Expression IP5 display at the Syngo workstation.<br> Model name and number: Philips Expression MR400"
+    "body": "Physiological monitoring equipment for participant stats (e.g., pulse, CO2, and blood pressure). This works together with the wireless Philips Expression IP5 display at the Syngo (MRI console) workstation.<br>Model name and number: Philips Expression MR400"
   },
   "NO_TITLE_BAD": {
     "body": "Oops, this should have a TITLE."
