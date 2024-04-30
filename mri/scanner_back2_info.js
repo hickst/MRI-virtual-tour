b99546f24f611fe5859b17919ac8f35887cb9a4b
Tom Hicks <hickst@tohono.com>
2024-04-29 19:59:44 -0700

    Add back2 infopoints: eyetracker, quenchvent, and CR door.

---
+++ mri/scanner_back2_info.js
@@ -0,0 +1,15 @@
+var tourInfo = {
+  "eyetracker": {
+    "title": "Eyetracker Camera",
+    "image": "/mri/images/sb2_eyetracker.jpg",
+    "body": "A black eyetracking camera is perched on top of the display trolley. It features an illuminator (left) and the camera (right). The eyetracker works in conjunction with an eye-tracking computer in the control room and transfers data over a fiber optic cable. Correct placement in the scanner bore is important to buffer the device from scanner vibrations.<br/>Model Name: Eyelink 1000 by SR Research"
+  },
+  "quenchVent": {
+    "title": "MRI Scanner Vent (aka Quench Vent)",
+    "body": "This large square pipe extending from the scanner to the ceiling can safely vent large amounts of super cold helium outside the building during a catastrophic emergency. A vent pipe must meet the diameter and pressure requirements of the MRI system manufacturer and be fully insulated."
+  },
+  "CRdoor": {
+    "title": "Door to the Control Room",
+    "body": "One enters and exits the Scanner room through this door, leading to the Control room. The door, itself, is sheathed in copper and must be closed while the scanner is in operation to prevent radio signals from interferring with the scanner."
+  },
+};
