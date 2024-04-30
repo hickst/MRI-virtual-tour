043947c32fc06afd1ea98a2272a06daaa41c2230
Tom Hicks <hickst@tohono.com>
2024-04-29 18:33:41 -0700

    Add breast coil closet infopoint.

--- mri/scanner_back1_info.js
+++ mri/scanner_back1_info.js
@@ -4,4 +4,9 @@ var tourInfo = {
     "image": "/mri/images/sb1_display_trolley.jpg",
     "body": "This positionable trolley holds a 24-inch HD monitor and eye-tracking glasses; both used for presenting visual stimuli. A custom aluminum enclosure makes the display MR-compatible. Its HD resolution (1900 x 1200 at 60Hz) is identical to the monitor in the console room and the two displays are cloned. However, using a specialized SD card, the image is left-right flipped so that it can be viewed by the participant through a mirror located on top of the head coil.<br/>Model Name: Cambridge Research Systems BOLDscreen MRI-compatible display."
   },
+  "breastCoil": {
+    "title": "Breast Coil Closet",
+    "image": "/mri/images/sb1_breast_coil_closet.jpg",
+    "body": "Closet opens to reveal a breast coil sitting on top of a small ladder."
+  },
 };
