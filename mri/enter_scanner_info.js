b07ee3fae7b3b44dad871e56e6cbad34528139b8
Tom Hicks <hickst@tohono.com>
2024-04-30 15:10:01 -0700

    Add ergometer to PP and ES.

--- mri/enter_scanner_info.js
+++ mri/enter_scanner_info.js
@@ -19,4 +19,9 @@ var tourInfo = {
     "image": "/mri/images/es_scan_table.jpg",
     "body": "The research participant lies on the scanner table, which is then moved horizontally into the bore for scanning. When completely out of the magnet, the table may also be moved vertically.<br/>Participant comfort is very important to minimizing movement and participants are made more comfortable with a variety of soft supports (e.g., for raising the knees, supporting the neck, or cushioning the head)."
   },
+  "ergometer": {
+    "title": "Ergometer",
+    "image": "/mri/images/es_ergometer.jpg",
+    "body": "In many diseases, symptoms will only occur during stress, therefore a dynamic evaluation is required. The MR ergometer is specifically designed for exercising, inducing stress during MRI examinations, in lieu of using drugs.<br/>Model Name: Lode MR Ergometer",
+  },
 };
--- mri/patch_panel_info.js
+++ mri/patch_panel_info.js
@@ -1,10 +1,12 @@
 var tourInfo = {
-  "MRI": {
-    "title": "MRI Research Scanner",
-    "body": "The MRI Research scanner is a 3T Siemens machine."
+  "scanTable": {
+    "title": "Scanner Table, Headphones, Coils, and the Bore",
+    "image": "/mri/images/es_scan_table.jpg",
+    "body": "The research participant lies on the scanner table, which is then moved horizontally into the bore for scanning. When completely out of the magnet, the table may also be moved vertically.<br/>Participant comfort is very important to minimizing movement and participants are made more comfortable with a variety of soft supports (e.g., for raising the knees, supporting the neck, or cushioning the head)."
   },
-  "MRItable": {
-    "title": "Scanner Table",
-    "body": "The research subject lies on this table to be scanned."
+  "ergometerInuse": {
+    "title": "Ergometer",
+    "image": "/mri/images/pp_ergometer_inuse.jpg",
+    "body": "In many diseases, symptoms will only occur during stress, therefore a dynamic evaluation is required. The MR ergometer is specifically designed for exercising to induce stress during MRI examinations, in lieu of using drugs.<br/>Model Name: Lode MR Ergometer<br/>Figure: Illustration of how the ergometer is used in the MRI scanner.",
   },
 };
