e0b44aa66cdc3627d4f34d9f2b6c3d90a536846f
Tom Hicks <hickst@tohono.com>
2024-04-30 15:55:09 -0700

    Add patch panel infopoint.

--- mri/patch_panel_info.js
+++ mri/patch_panel_info.js
@@ -9,4 +9,9 @@ var tourInfo = {
     "image": "/mri/images/pp_ergometer_inuse.jpg",
     "body": "In many diseases, symptoms will only occur during stress, therefore a dynamic evaluation is required. The MR ergometer is specifically designed for exercising to induce stress during MRI examinations, in lieu of using drugs.<br/>Model Name: Lode MR Ergometer<br/>Figure: Illustration of how the ergometer is used in the MRI scanner.",
   },
+  "patchpanel": {
+    "title": "Patch Panel",
+    "image": "/mri/images/pp_patchpanel.jpg",
+    "body": "The patch panel allows cables to pass from the console room to the scanner room while blocking radio frequencies that would negatively impact scans. Note the scanner room's copper shielding showing around the panel edge and the three copper tubes (top center and bottom left & right) for passing cables through. The copper tubes are carefully designed so that problem radio frequencies are blocked.",
+  },
 };
