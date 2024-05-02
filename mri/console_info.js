05aeb90348eaa3cca7ba720fb53ca19957d34c34
Tom Hicks <hickst@tohono.com>
2024-05-01 17:24:27 -0700

    Update console page and add infopoints.

---
+++ mri/console_info.js
@@ -0,0 +1,17 @@
+var tourInfo = {
+  "blankets": {
+    "title": "Blanket Warmer",
+    "image": "/mri/images/cr_blankets.jpg",
+    "body": "This blanket warmer allows us to offer a clean warm blanket to a participant if they feel cold.<br/>Model Name: Enthermics 4-Blanket Warming Cabinet (DC150)",
+  },
+  "MRglasses": {
+    "title": "MRI-Compatible Glasses",
+    "image": "/mri/images/cr_MRglasses.jpg",
+    "body": "MR-safe prescription glasses (with no metallic components). Glasses are adjustable, with -6 to +6 diopter lenses in 0.5 diopter increments. If a participant has eyesight problems (and you have an fMRI task that requires good vision), you can ask them to bring their prescription to the session. Allow extra preparation time to find suitable lenses.<br/>Model Name: Cambridge Research Systems MR-Compatible Glasses for Vision Correction",
+  },
+  "computerRoom": {
+    "title": "Computer Room",
+    "image": "/mri/images/cr_computer_room.jpg",
+    "body": "The computer room, where the XNAT server and other computers are kept. Researchers can request access to use these machines."
+  },
+};
