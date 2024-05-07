7feab32b5d0ee76c26cf6812eb6d6e1b4002e4ca
Tom Hicks <hickst@tohono.com>
2024-05-06 19:31:22 -0700

    Add changing room infosphere to console. Change popup close buttons to grey.

--- mri/console_info.js
+++ mri/console_info.js
@@ -4,6 +4,11 @@ var tourInfo = {
     "image": "../images/cr_copper_door.jpg",
     "body": "One enters and exits the Scanner room through this door. The door is sheathed in copper and must be closed while the scanner is in operation to prevent radio signals from interfering with the scanner."
   },
+  "changeRooms": {
+    "title": "Changing Rooms",
+    "image": "../images/cr_changing_rooms.jpg",
+    "body": "There are two changing rooms available for participants to secure their belongings. All metal or magnetic items (e.g., belts, keys, sunglasses, wallets, watches, jewelry) must be left in a locker. The special locker key <b>IS</b> MRI safe and can be taken into the scanner room."
+  },
   "blankets": {
     "title": "Blanket Warmer",
     "image": "../images/cr_blankets.jpg",
