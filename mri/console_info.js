012e85921732b246255b07d5bfee89f09376af1d
Tom Hicks <hickst@tohono.com>
2024-05-07 12:09:13 -0700

    Scanner room warning: finish with new, separate warning popup.

--- mri/console_info.js
+++ mri/console_info.js
@@ -1,8 +1,8 @@
 var tourInfo = {
   "metalWarning": {
-    "title": "Danger! No Metal Allowed!",
+    "title": "Danger: No Metal Allowed!",
     "image": "../images/cr_copper_door.jpg",
-    "body": '<p class="danger"><b>ALL metal MUST be removed before entering the scanner room.</b><br/>This includes common items like belts, keys, watches, glasses, wallets, pens, hairclips, and jewelry. Also remove any magnetic materials (e.g., credit and ID cards, USB keys) from your person.</p>'
+    "body": '<b>ALL metal MUST be removed before entering the scanner room.</b><br/><br/>This includes common items like belts, keys, watches, glasses, wallets, pens, hairclips, and jewelry. Also remove any magnetic materials (ID cards, credit cards, USB keys, etc.) from your person.'
   },
   "scanDoor": {
     "title": "Scanner Room Door",
--- mri/tcomponents.js
+++ mri/tcomponents.js
@@ -1,5 +1,6 @@
 // const sleepNow = (delay) => new Promise((resolve) => setTimeout(resolve, delay))
 
+// Creates a modal iFrame which pops up to show the given information.
 function showInfoPopup (info) {
   let theIFrame = document.querySelector("#the-iframe");
   // console.log("theIFrame:", theIFrame);     // for DEBUG
@@ -57,6 +58,40 @@ function showInfoPopup (info) {
 };
 
 
+// Creates a modal iFrame which pops up to show the given warning information.
+function showWarnPopup (info) {
+  let warnFrame = document.querySelector("#warn-iframe");
+
+  if (warnFrame.style.display == "block") {
+    warnFrame.style.display = "none";
+  } else {
+    let wTitle = warnFrame.contentDocument.querySelector("#warn-title");
+    if (info.title === undefined) {
+      wTitle.innerText = "No title found in the '_info.js' data file";
+    } else {
+      wTitle.innerText = info.title;
+    };
+
+    let wImage = warnFrame.contentDocument.querySelector("#warn-image");
+    if (info.image) {
+      wImage.setAttribute("src", info.image);
+      wImage.style.visibility = null;
+    } else {
+      wImage.style.visibility = "hidden";
+    };
+
+    let wBody = warnFrame.contentDocument.querySelector("#warn-body");
+    if (info.body === undefined) {
+      wBody.innerHTML = "No body info found in the '_info.js' data file";
+    } else {
+      wBody.innerHTML = info.body;
+    };
+
+    warnFrame.style.display = "block";
+  };
+};
+
+
 // Listen for cursor clicks, modify colors of clicked 3D model object, and report
 // click event details to the browser console. NB: for testing only, not used in tour.
 AFRAME.registerComponent('click-listener', {
@@ -94,12 +129,12 @@ AFRAME.registerComponent('plane-warn', {
     mouseenter: function (ev) {
       this.el.setAttribute("material", "opacity", "0.5");
       var info = tourInfo["metalWarning"];
-      showInfoPopup(info);
+      showWarnPopup(info);
       // console.log("Enter EL:", this.el);   // for DEBUG
     },
     mouseleave: function (ev) {
       this.el.setAttribute("material", "opacity", "0.2");
-      document.querySelector("#the-iframe").style.display = "none";
+      document.querySelector("#warn-iframe").style.display = "none";
       // console.log("Leave EL:", this.el);   // for DEBUG
     },
   }
