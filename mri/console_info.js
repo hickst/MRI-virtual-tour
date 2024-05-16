158dea246563b23e737e2ef873dc722956e39888
Tom Hicks <hickst@tohono.com>
2024-05-15 18:30:20 -0700

    Bootstrap modals: console scene, resize some images. Retain warning iframe for now.

--- mri/console_info.js
+++ mri/console_info.js
@@ -4,29 +4,4 @@ var tourInfo = {
     "image": "../images/cr_copper_door.jpg",
     "body": '<b>ALL metal MUST be removed before entering the Scanner room.</b><br/><br/>This includes common items like belts, keys, watches, glasses, wallets, pens, hairclips, and jewelry. Also remove any magnetic materials (ID cards, credit cards, USB keys, etc.) from your person.'
   },
-  "scanDoor": {
-    "title": "Scanner Room Door",
-    "image": "../images/cr_copper_door.jpg",
-    "body": 'One enters and exits the Scanner room through this door. The door is sheathed in copper and must be closed while the scanner is in operation to prevent radio signals from interfering with the scanner.<br/><br/><p class="danger"><b>ALL metal MUST be removed before entering the Scanner room.</b><br/>This includes common items like belts, keys, watches, glasses, wallets, pens, hairclips, and jewelry. Also remove any magnetic materials (e.g., credit and ID cards, USB keys) from your person.</p>'
-  },
-  "changeRooms": {
-    "title": "Changing Rooms",
-    "image": "../images/cr_changing_room.jpg",
-    "body": "There are two changing rooms available for participants to secure their belongings. All metal or magnetic items (e.g., belts, keys, watches, glasses, wallets, jewelry) must be left in a locker. The special locker keys <b>ARE</b> MRI safe and can be taken into the Scanner room."
-  },
-  "blankets": {
-    "title": "Blanket Warmer",
-    "image": "../images/cr_blankets.jpg",
-    "body": "This blanket warmer allows us to offer a clean warm blanket to a participant if they feel cold.<br/><br/>Model Name: Enthermics 4-Blanket Warming Cabinet (DC150)",
-  },
-  "MRglasses": {
-    "title": "MRI-Compatible Glasses",
-    "image": "../images/cr_MRglasses.jpg",
-    "body": "MR-safe prescription glasses (with no metallic components). Glasses are adjustable, with -6 to +6 diopter lenses in 0.5 diopter increments. If a participant has eyesight problems (and you have an fMRI task that requires good vision), you can ask them to bring their prescription to the session. Allow extra preparation time to find suitable lenses.<br/><br/>Model Name: Cambridge Research Systems MR-Compatible Glasses for Vision Correction",
-  },
-  "computerRoom": {
-    "title": "Computer Room",
-    "image": "../images/cr_computer_room.jpg",
-    "body": "The computer room, where the XNAT server and other computers are kept. Researchers can request access to use these machines."
-  },
 };
